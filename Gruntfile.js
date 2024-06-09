8818ec30020c9b1b678ed1d3e24aad729ad8f0bc
data-miner00 <data-miner00@users.noreply.github.com>
2024-06-09 14:54:40 +0800

    refactor: Move tutorial tasks out from Gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,16 +10,6 @@ module.exports = function (grunt) {
   // Register tasks
   grunt.loadTasks("tasks");
 
-  grunt.registerTask("run", function () {
-    console.log("Running from grunt");
-  });
-
-  grunt.registerTask("run2", function () {
-    console.log("Running from grunt 2");
-  });
-
-  grunt.registerTask("test-env", ["run", "run2"]);
-
   // Run standalone concat tasks
   grunt.registerTask("concat-js-only", ["concat:js"]);
   grunt.registerTask("concat-css-only", ["concat:css"]);
